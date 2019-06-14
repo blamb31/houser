9c451bc887319173cd8a5e57e243d0ef7191cd71
Blake Lamb <blake.lamb31@gmail.com>
2019-06-14 16:33:02 -0600

    Minor changes

--- src/Components/House/House.js
+++ src/Components/House/House.js
@@ -14,7 +14,7 @@ class House extends Component {
                 payload: res.data
             })
         }
-        ).catch(res.sendStatus(400)
+        ).catch(()) => console.log('error code 400'))
     }
     render() {
         let {property} = this.props
